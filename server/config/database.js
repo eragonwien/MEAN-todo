33d868a31cba0439c14eaa0546aabdaafc7dd516
SonLaptop\erago <eragonwien@gmail.com>
2018-01-09 11:59:22 +0100

    delete unused files

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,11 @@
-exports.DB_HOST = "localhost";
-exports.DB_USER = "eragonwien";
-exports.DB_PASSWORD = "1212";
-exports.DB_TEST = "test";
-exports.DB_PRODUCTION = "production";
-exports.DB_POOL_LIMIT = 100; // number of connections per pool
-exports.DB_NUM_OF_FAKE_DATA = 100;
+var config = {
+    DB_HOST: 'localhost',
+    DB_USER: 'eragonwien',
+    DB_PASSWORD: '1212',
+    DB_TEST: 'test',
+    DB_PRODUCTION: 'production',
+    DB_POOL_LIMIT: 100, // number of connections per pool
+    DB_NUM_OF_FAKE_DATA: 100
+}
+
+exports.module = config;
