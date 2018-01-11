347d1ab15b8fb751e6e7bd48d9273338f0453327
SonLaptop\erago <eragonwien@gmail.com>
2018-01-11 18:58:58 +0100

    add constraint for dev mode

--- server/models/connect.js
+++ server/models/connect.js
@@ -2,12 +2,11 @@ var dotenv = require('dotenv').config();
 var mysql = require('mysql');
 var db = require('../config/database');
 
-
 exports.pool = mysql.createPool({
   host     : db.DB_HOST,
   user     : db.DB_USER,
   password : db.DB_PASSWORD,
-  database : (process.env.NODE_ENV === 'development') ? db.DB_TEST : db.DB_PRODUCTION,
+  database : (process.env.NODE_ENV !== 'production') ? db.DB_TEST : db.DB_PRODUCTION,
   connectionLimit: db.DB_POOL_LIMIT,
   //debug: (process.env.NODE_ENV === 'development'), 
   multipleStatements: true
