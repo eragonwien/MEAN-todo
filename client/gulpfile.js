8f5b1eab97ac1c4e5818edd69fbe057885e59657
SonLaptop\erago <eragonwien@gmail.com>
2018-01-24 12:57:35 +0100

    building first file structure

---
+++ client/gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var userref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+var gulpIf = require('gulp-if');
+var plumber = require('gulp-plumber');
+gulp.task('sass', function () {
+    return gulp.src('src/sass/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('public'))
+});
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        port: 3000,
+        server: {
+            baseDir: 'public'
+        }
+    });
+});
+
+gulp.task('browser-sync-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('minijs', function () {
+    return gulp.src('src/index.html')
+        .pipe(plumber())    
+        .pipe(userref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulp.dest('public'))   
+});
+
+gulp.task('public', ['browser-sync', 'sass', 'minijs']);
+
+gulp.task('watch', ['browser-sync', 'sass'], function () {
+    gulp.watch('src/sass/**/*.scss', ['sass', 'browser-sync-reload']);
+    gulp.watch('src/index.html', ['minijs', 'browser-sync-reload']);
+    gulp.watch('src/js/**/*.js', ['minijs', 'browser-sync-reload']);
+});
