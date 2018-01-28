f6d1b2b9d710b2ed34bb7ae6d1da252e2be7bcaf
SonLaptop\erago <eragonwien@gmail.com>
2018-01-28 13:25:48 +0100

    remove default field

--- server/config/database.js
+++ server/config/database.js
@@ -1,11 +1,11 @@
 var config = {
-    DB_HOST: 'localhost',
-    DB_USER: 'eragonwien',
-    DB_PASSWORD: '1212',
-    DB_TEST: 'test',
-    DB_PRODUCTION: 'production',
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_TEST: process.env.DB_NAME_TEST,
+    DB_PRODUCTION: process.env.DB_NAME_PRODUCTION,
     DB_POOL_LIMIT: 100, // number of connections per pool
-    DB_NUM_OF_FAKE_DATA: 100
+    DB_NUM_OF_FAKE_DATA: 100 // used for test generator
 }
 
 module.exports = config;
