b68a763b773754b5d41a964b226e636898216809
SonLaptop\erago <eragonwien@gmail.com>
2018-01-29 12:27:24 +0100

    restructure, add gulp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -6,21 +6,8 @@ var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
 var gulpIf = require('gulp-if');
 var plumber = require('gulp-plumber');
-gulp.task('sass', function () {
-    return gulp.src('src/sass/*.scss')
-        .pipe(plumber())
-        .pipe(sass())
-        .pipe(gulpIf('*.css', cssnano()))
-        .pipe(gulp.dest('public'))
-});
 
-gulp.task('minicss', function () {
-    return gulp.src('public/stylesheets')
-        .pipe(plumber())
-        .pipe(gulpIf('*.css', cssnano()))
-        .pipe(gulp.dest('public/stylesheets'))
-});
-
+/* browser sync */
 gulp.task('browser-sync', function () {
     browserSync.init({
         port: 4000,
@@ -34,7 +21,17 @@ gulp.task('browser-sync-reload', function () {
     browserSync.reload();
 });
 
-gulp.task('minijs', function () {
+/* sass and minify css */
+gulp.task('sass', function () {
+    return gulp.src('src/sass/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('public'))
+});
+
+/* minify js and bundling */
+gulp.task('js', function () {
     return gulp.src('src/index.html')
         .pipe(plumber())    
         .pipe(userref())
@@ -42,18 +39,25 @@ gulp.task('minijs', function () {
         .pipe(gulp.dest('public'))   
 });
 
+/* minify images */
+
+/* gzip */
+
+/* other transfer to public */
 gulp.task('favicon', function () {
     return gulp.src('src/favicon.ico')
         .pipe(gulp.dest('public'))
 });
 
-gulp.task('bundle', ['sass', 'minijs', 'favicon']);
+/* public */
+gulp.task('public', ['sass', 'js', 'favicon']);
 
+/* transfer to docs */
+gulp.task('docs', function () {
+    return gulp.src('public/*')
+        .pipe(gulp.dest('../docs'))
+})
+/* watch */
 gulp.task('watch', ['minicss'], function () {
     gulp.watch('public/stylesheets/style.css', ['minicss']);
-});
-
-gulp.task('public', ['bundle'], function () {
-    return gulp.src('public/*')
-    .pipe(gulp.dest('../docs'))
 });
