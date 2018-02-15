89eaa879efffdd46f4d55b275c16fde7e2b5c071
eragonwien <nguyennhoangson@gmail.com>
2018-02-15 05:11:27 +0100

    add loading spinner

--- app/public/javascripts/components/main/mainController.js
+++ app/public/javascripts/components/main/mainController.js
@@ -17,6 +17,7 @@ function mainController(sc, loginService, projectService, todoService) {
     getAllProjects();
 
     function getAllProjects() {
+        vm.loading = true;
         return projectService.getAllProjectsByUid(vm.user.Uid).then(getAllProjectsHandler);
 
         function getAllProjectsHandler(response) {
@@ -32,6 +33,7 @@ function mainController(sc, loginService, projectService, todoService) {
         
                 function getTodosHandler(response) {
                     applyTodosToProjects(response.data, projects);
+                    vm.loading = false;
                     return response.data;
                 }
             }
@@ -50,20 +52,24 @@ function mainController(sc, loginService, projectService, todoService) {
     }
 
     function createNewProject(project) {
+        vm.loading = true;
         projectService.createNewProject(vm.user.Uid, project).then(createNewProjectHandler);
 
         function createNewProjectHandler(response) {
             // refresh list of project
             setAlert('Project created.', true);
             vm.newProject = {};
+            vm.loading = false;
             getAllProjects();
         }
     }
 
     function deleteProject(project) {
+        vm.loading = true;
         projectService.deleteProjectById(project.Pid, project.Uid).then(deleteProjectHandler);
 
         function deleteProjectHandler(response) {
+            vm.loading = false;
             // refresh list of project
             setAlert('Project deleted.', true);
             var index = vm.projects.indexOf(project);
@@ -72,9 +78,11 @@ function mainController(sc, loginService, projectService, todoService) {
     }
 
     function createTodo(project, todo) {
+        vm.loading = true;
         todoService.createTodo(project, todo).then(createTodoHandler);
 
         function createTodoHandler(response) {
+            vm.loading = false;
             // refresh list of project
             //setAlert('Todo added to ' + project.Name, true);
             todo = angular.copy(response.config.data);
@@ -85,9 +93,11 @@ function mainController(sc, loginService, projectService, todoService) {
     }
 
     function deleteTodo(project, todo) {
+        vm.loading = true;
         todoService.deleteTodo(project, todo).then(deleteTodoHandler);
 
         function deleteTodoHandler(response) {
+            vm.loading = false;
             setAlert('Todo deleted.', true);
             var index = project.todos.indexOf(todo);
             project.todos.splice(index, 1);
@@ -105,9 +115,11 @@ function mainController(sc, loginService, projectService, todoService) {
     }
 
     function updateTodo(todo) {
+        vm.loading = true;
         todoService.updateTodo(todo).then(updateTodoHandler);
 
         function updateTodoHandler(response) {
+            vm.loading = false;
             //setAlert('Todo Nr.' + todo.Tid + ' is updated.', true);
             var progress = response.data.projectProgress;
             vm.projects.forEach(updateProgress);
